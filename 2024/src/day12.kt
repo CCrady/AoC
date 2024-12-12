b43e1f0bdb16aa45ee64b1683d07398f468159ba
Calvin Crady <calvin.crady@gmail.com>
2024-12-12 01:50:08 -0800

    Add day 12 part 2

--- 2024/src/day12.kt
+++ 2024/src/day12.kt
@@ -1,18 +1,48 @@
 import java.io.File
 
-fun main() = solve("12", ::parse, ::part1)
+fun main() = solve("12", ::parse, ::part1, ::part2)
 
 private fun parse(file: File): Matrix<Char> = Matrix(file.readLines()) { _, c -> c }
 
 private fun part1(atlas: Matrix<Char>): Int {
-    return regions(atlas).sumOf { region -> region.price }
+    return regions(atlas).sumOf { region -> region.area * region.perimeter }
 }
 
-private data class Region(val plots: Collection<Vec2>, val perimeter: Int) {
+private fun part2(atlas: Matrix<Char>): Int {
+    return regions(atlas).sumOf { region -> region.area * region.numSides() }
+}
+
+private data class Region(val plots: Collection<Vec2>, val edges: Collection<Edge>) {
     val area: Int
         get() = plots.size
-    val price: Int
-        get() = perimeter * area
+    val perimeter: Int
+        get() = edges.size
+    fun numSides(): Int {
+        var numSidesAcc = 0
+        val uncountedEdges = edges.toMutableSet()
+        // iterate through uncountedEdges, and remove all contiguous edges with the same orientation
+        while (uncountedEdges.isNotEmpty()) {
+            numSidesAcc++
+
+            // pop an arbitrary edge from uncountedEdges
+            val edge = uncountedEdges.first()
+            uncountedEdges.remove(edge)
+            // remove all contiguous edges with the same orientation in the given direction
+            fun runningRemove(dir: Vec2.CardinalDirection) {
+                var toRemove = edge.nudge(dir)
+                while (uncountedEdges.remove(toRemove)) {
+                    toRemove = toRemove.nudge(dir)
+                }
+            }
+            runningRemove(edge.orientation.turnCW)
+            runningRemove(edge.orientation.turnCCW)
+        }
+        return numSidesAcc
+    }
+}
+
+private data class Edge(val pos: Vec2, val orientation: Vec2.CardinalDirection) {
+    fun nudge(dir: Vec2.CardinalDirection): Edge = Edge(pos + dir, orientation)
 }
 
 private fun regions(atlas: Matrix<Char>): Sequence<Region> = sequence {
@@ -25,7 +55,7 @@ private fun regions(atlas: Matrix<Char>): Sequence<Region> = sequence {
 
 private fun floodFill(startPos: Vec2, char: Char, atlas: Matrix<Char>, visited: MutableMatrix<Boolean>): Region {
     val plots = ArrayDeque<Vec2>()
-    var perimeterAcc = 0
+    val edges = ArrayDeque<Edge>()
     val stack = ArrayDeque(listOf(startPos))
     visited[startPos] = true
     while (stack.isNotEmpty()) {
@@ -34,12 +64,12 @@ private fun floodFill(startPos: Vec2, char: Char, atlas: Matrix<Char>, visited: 
         for (dir in Vec2.CardinalDirection.entries) {
             val newPos = currPos + dir
             if (!atlas.inBounds(newPos) || atlas[newPos] != char) { // we found an edge
-                perimeterAcc++
+                edges.add(Edge(currPos, dir))
             } else if (!visited[newPos]) { // newPos has yet to be queued
                 visited[newPos] = true
                 stack.addLast(newPos)
             }
         }
     }
-    return Region(plots, perimeterAcc)
+    return Region(plots, edges)
 }
